0e6498335d1bf7008275ccb5e860edf467df96ae
Gustavo <gustavohique12@gmail.com>
2024-01-07 21:26:47 -0300

    fix: adicionada barra que faltava.

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -5,7 +5,7 @@ import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { createClient } from 'graphql-ws';
 
 const httpLink = new HttpLink({
-  uri: `http:${process.env.REACT_APP_API_URL}graphql`
+  uri: `http:${process.env.REACT_APP_API_URL}/graphql`
 });
 
 const wsLink = new GraphQLWsLink(createClient({
