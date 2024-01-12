8c59d94c5b974db78f094aea6f56d629fce54fb5
Gustavo <gustavohique12@gmail.com>
2024-01-12 19:57:39 -0300

    feat: Ajustado layout para a tela de envio da solicitação.

--- src/pages/BartenderAuth/BartenderAuth.tsx
+++ src/pages/BartenderAuth/BartenderAuth.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
-import '../Login/login.scss';
 import './bartenderAuth.scss';
 import Loading from "../../components/Loading";
 
